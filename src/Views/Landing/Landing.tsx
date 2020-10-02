9a5bb655112574cade1f3d4dcc68e4ebcad3f3ed
benja <benjaminglejeune@gmail.com>
2020-10-02 15:33:41 +0100

    Removed test colours on the borders/sections, altered animations, landing text now moves to avoid red rectangle

--- src/Views/Landing/Landing.tsx
+++ src/Views/Landing/Landing.tsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './landing.css';
+import './landingAnimation.css';
 import TitleGreeting from "./TitleGreeting";
 import TitleName from "./TitleName";
 
 export default function Landing() {
 
-    return <div id="LandingView">
-        <div id="LandingBoldTitle">
-            <TitleGreeting/>
-            <TitleName/>
+    useEffect(() => {
+        window.addEventListener('scroll', onScroll);
+        return () => window.removeEventListener('scroll', onScroll);
+    })
+
+    const onScroll = (e:Event) => {
+        const scroll = window.scrollY
+        const redRect = document.getElementById("LandingView");
+
+        if ( redRect ) {
+            console.log(scroll);
+            if ( scroll > 0 && redRect.className === 'anim-dormant') {
+                redRect.className = 'anim-transition';
+                setTimeout( () => { if ( redRect.className !== 'anim-dormant' ) redRect.className = 'anim-final' }, 500 )
+            }
+            if (scroll === 0) {
+                redRect.className = 'anim-dormant';
+            }
+        }
+    }
+
+    return <div id="LandingView" className="anim-dormant">
+        <div id="LandingTitleContainer">
+            <div id="LandingBoldTitle">
+                <TitleGreeting/>
+                <TitleName/>
+            </div>
+        </div>
+        <div id="LandingTransitionRect">
+
         </div>
     </div>
 
